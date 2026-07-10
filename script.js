baa9554a8d360667845a686c6a7b33b4be7d5301
Mobin <mobin.web3.dev@gmail.com>
2026-07-10 12:36:29 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function searchTXID(){
 
+    const txid = document.getElementById("txidInput").value;
+
+    if(txid === ""){
+
+        alert("Please enter a Transaction ID.");
+
+        return;
+
+    }
+
+    document.getElementById("result").innerHTML = `
+        <h2>Searching...</h2>
+        <p>Transaction ID: ${txid}</p>
+    `;
+
+}
